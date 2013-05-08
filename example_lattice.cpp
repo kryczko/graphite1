d9831fc29b0f24dca30e56fc603bce6d62cd95ad
Kevin Ryczko <kryczko@viz1-uoit.sharcnet.ca>
2013-05-08 17:38:13 -0400

    Updated version of example_lattice.cpp

--- example_lattice.cpp
+++ example_lattice.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
 
+
+int rows, columns;
+string outputfile;
+
+cout << "\nWelcome to the Carbon Lattice Generator.\n\n";
+cout << "How many rows of carbon atoms would you like: ";
+cin >> rows;
+cout << "How many columns of carbon atoms would you like: ";
+cin >> columns; 
+cout << "What is the name of the output file (e.g. data.xyz): ";
+cin >> outputfile;
+
 ofstream out;
-out.open("example_data.xyz");
+out.open(outputfile.c_str());
 
-out << 41  << "\n\n";
 
-double c0_x = -2.0, c0_y = -2.0, c1_x = -1.0, c1_y = -1.0;
+out << 2*rows*columns  << "\n\n";
 
-double x0_values[5], y0_values[5], x1_values[4], y1_values[4];
+double c0_x = -2.0, c0_y = -2.0, c1_x, c1_y;
 
-for (int i = 0; i < 5; i ++)
+double x0_values[columns], y0_values[rows], x1_values[columns], y1_values[rows];
+
+for (int i = 0; i < columns; i ++)
 {	
 	c0_x += 2.0;
-	
+	c1_x = c0_x + 1.0;
 	x0_values[i] = c0_x;
-	
+	x1_values[i] = c1_x;
+
 	c0_y = -2.0;
-	for (int n = 0; n < 5; n ++)
+	for (int n = 0; n < rows; n ++)
 	{
 		c0_y += 2.0;
+		c1_y = c0_y + 1.0;
 		y0_values[n] = c0_y;
+		y1_values[n] = c1_y;
 		out << 	"C\t" << x0_values[i] << "\t" << y0_values[n] << "\t" << 0.0 << endl;
+		out << "C\t" << x1_values[i] << "\t" << y1_values[n] << "\t" << 0.0 << endl;
 	}
 }
-for (int j = 0; j < 4; j ++)
-{
-        c1_x += 2.0;
-
-        x1_values[j] = c1_x;
-
-        c1_y = -1.0;
-        for (int k = 0; k < 4; k ++)
-        {
-                c1_y += 2.0;
-                y1_values[k] = c1_y;
-                out <<  "C\t" << x1_values[j] << "\t" << y1_values[k] << "\t" << 0.0 << endl;
-        }
-}
-
 
 out.close();
 return 0;
